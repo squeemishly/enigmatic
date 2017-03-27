ce1fcfc80771bfd3d7a34f4a0acaa7ce9cdedbed
Yohanan Assefa <nanahoy@gmail.com>
2017-03-27 15:05:38 -0600

    work in progress

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,28 +1,54 @@
-require './lib/key' #take out after testing
 require 'pry'
+require './lib/key.rb'
 
 class Encryption
   attr_reader :new_key, :char_map
   attr_accessor :message
+
   def initialize(message)
     @new_key = Key.new
-    @char_map = ('a'..'z').to_a + (1..9).to_a + ['.', ',', ' ']
+    @char_map = ('a'..'z').to_a + ("0".."9").to_a + ['.', ',', ' ']
     @message = message
+    @encrypted = ""
   end
 
-  def create_chars
-    @message = message.chars
+  def create_splits
+    @message = message.downcase.chars
   end
 
-  def create_splits(message)
-    message = message.chars.each_slice(4).to_a
+  def encrypt
+    create_splits.each_slice(4) do |slice|
+      slice.each_with_index do |message, ind|
+        p message_index = @char_map.index(message)
+        p @encrypted << @char_map.rotate(@new_key.key[ind])[message_index]
+      end
+    end
+    @encrypted
   end
 end
 
 message = Encryption.new("Hello, World")
-message.create_chars
-#split_message = message.create_splits("Hello, World")
-
-
 binding.pry
-""
+"------------"
+
+      # message = Encryption.new("Hello, World")
+      # # message.create_chars
+      # message.new_key.merge_methods
+      # split_message = message.create_splits
+      # zipped_message = split_message.map do |arr|
+      # arr.zip(message.new_key.key_collection)
+      # end
+      # zipped_message.flatten!(1)
+      # new_array = []
+      # zipped_message.each_index do |index|
+      # new_array << message.char_map.index(zipped_message[index][0])
+      # end
+      # numbers = []
+      # new_array.each_index do |index|
+      # numbers << (new_array[index]+zipped_message[index][1])
+      # end
+      # coded_message = ""
+      # numbers.each do |num|
+      # coded_message << message.char_map.rotate(num)[0]
+      # end
+      # puts coded_message
--- test/key_test.rb
+++ test/key_test.rb
@@ -4,23 +4,28 @@ require './lib/key'
 require 'Date'
 
 class KeyTest < Minitest::Test
+
   def test_it_exists
     new_key = Key.new
+
     assert_instance_of Key, new_key
   end
 
   def test_it_creates_random_values
     new_key = Key.new
-    assert_instance_of Fixnum, new_key.create_five_digit_number
+
+    assert_instance_of Fixnum, new_key.random_five_digit_number
   end
 
   def test_it_splits_the_numbers_to_values
     new_key = Key.new
-    new_key.split_five_digit_number
-    assert_equal 56, new_key.a
-    assert_equal 67, new_key.b
-    assert_equal 78, new_key.c
-    assert_equal 89, new_key.d
+    expected = new_key.rotation
+
+    assert_instance_of Array, expected
+    assert_equal 4, expected.length
+    expected.each do |num|
+      assert_instance_of Fixnum, num
+    end
   end
 
   def test_last_four_digits_of_date_squared_is_the_offset
@@ -28,14 +33,15 @@ class KeyTest < Minitest::Test
     assert_equal [0, 4, 8, 9], new_key.offset
   end
 
-  def test_add_ABCD_together
+  def test_offset_combines_rotation_and_last_digits_of_date
     new_key = Key.new
-    new_key.split_five_digit_number
-    new_key.offset
-    new_key.add_offset_to_split
-    assert_equal 56, new_key.a
-    assert_equal 71, new_key.b
-    assert_equal 86, new_key.c
-    assert_equal 98, new_key.d
+    expected = new_key.offset
+
+    assert_instance_of Array, expected
+    assert_equal 4, new_key.offset.length
+    expected.each do |num|
+      assert_instance_of Fixnum, num
+    end
   end
+
 end
